1e73f6cb4219defd2e158c1af208d63e03dbfb8f
mm20 <maxmustermannbsp@gmail.com>
2016-07-25 00:46:02 +0200

    svg2png: create png file in $size dir

--- svg2png.sh
+++ svg2png.sh
@@ -1,7 +1,8 @@
 # A tool to batch convert svg files to png files. Just execute ./svg2png.sh. Needs inkscape to be installed.
 #!/bin/sh
-size=512
+size=96
+mkdir -p $size/svg
 for file in svg/*.svg
 do
-     /usr/bin/inkscape -f "${file}" -w $size -e "png/${file}.png"
+     /usr/bin/inkscape -f "${file}" -w $size -e "$size/${file}.png"
 done
